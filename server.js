1d2ac5c70cb2eec157ad7befcb39bf015ffb6cb2
akh672 <121810313059@gitam.edu>
2023-09-24 10:00:18 +0530

    Added Cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
-// Serve static files
-app.use(express.static(__dirname));
-app.use('/', express.static('public'));
+
+// // Serve static files
+// app.use(express.static(__dirname));
+// app.use('/', express.static('public'));
+
+app.use(cors());
 // Endpoint to serve budget.json
 app.get('/budget', (req, res) => {
     fs.readFile('budget.json', 'utf8', (err, data) => {
@@ -18,5 +22,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
